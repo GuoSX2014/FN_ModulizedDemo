ee7fc715a55667ff507d09473868adaf7c96a42d
guoshixu <guoshixu@enn.cn>
2019-01-03 15:38:54 +0800

    初始化Demo

---
+++ FN_ModulizedDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FN_ModulizedDemo
+//
+//  Created by guoshixu on 2019/1/3.
+//  Copyright © 2019 fanneng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FN_ModulizedDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FN_ModulizedDemo
+//
+//  Created by guoshixu on 2019/1/3.
+//  Copyright © 2019 fanneng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
